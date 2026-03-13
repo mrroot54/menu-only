2ca3bb56e5cf9c32e141b5e938c43dccff73cdc5
mrroot54 <mrroot543@gmail.com>
2026-03-13 12:02:48 +0530

    fix user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,7 +5,6 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-use function fake;
 
 class UserFactory extends Factory
 {
@@ -14,8 +13,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(), // Changed fake() to $this->faker
+            'email' => $this->faker->unique()->safeEmail(), // Changed fake() to $this->faker
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
