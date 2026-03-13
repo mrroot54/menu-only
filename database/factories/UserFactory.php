c1d1bb9718054e34fc8b2696d906a4cf8b57d3aa
mrroot54 <mrroot543@gmail.com>
2026-03-13 11:53:38 +0530

    fix fake helper import

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use function fake;   // 👈 ye line add karo
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
